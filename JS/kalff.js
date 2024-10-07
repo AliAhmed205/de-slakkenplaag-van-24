93eb15b897f11d4dc66f7f26694c1098e697bb28
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-10-07 13:15:32 +0200

    Day 7 added + descriptions for each finding of every day. Finished kalf-section

--- JS/kalff.js
+++ JS/kalff.js
@@ -13,9 +13,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const dayData = {
     1: {
       date: '26/09/2024',
-      temperature: '11°C',
+      temperature: '15°C',
       precipitation: '14.4mm',
-      slugs: 6
+      slugs: 7
     },
     2: {
       date: '30/09/2024',
@@ -27,17 +27,17 @@ document.addEventListener("DOMContentLoaded", function () {
       date: '01/10/2024',
       temperature: '12°C',
       precipitation: '6.1mm',
-      slugs: 1
+      slugs: 4
     },
     4: {
       date: '02/10/2024',
-      temperature: '10°C',
+      temperature: '12°C',
       precipitation: '2.6mm',
-      slugs: 4
+      slugs: 3
     },
     5: {
       date: '03/10/2024',
-      temperature: '8°C',
+      temperature: '6°C',
       precipitation: '0mm',
       slugs: 1
     },
@@ -45,13 +45,13 @@ document.addEventListener("DOMContentLoaded", function () {
       date: '04/10/2024',
       temperature: '5°C',
       precipitation: '0mm',
-      slugs: 2
+      slugs: 3
     },
     7: {
       date: '07/10/2024',
-      temperature: '20°C',
-      precipitation: '5mm',
-      slugs: 2
+      temperature: '14°C',
+      precipitation: '0mm',
+      slugs: 6
     },
     8: {
       date: '08/10/2024',
@@ -63,7 +63,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Marker data voor elke dag
   const markersData = {
-    1: [{
+    1: [
+      {
+        top: '90%',
+        left: '46%',
+        image: './IMG/day-1/IMG_4216.png',
+        type: 'foto',
+        description: 'Het is vandaag 15°C'
+      },
+      {
         top: '60%',
         left: '60%',
         image: './IMG/day-1/IMG_4198.png',
@@ -81,6 +89,13 @@ document.addEventListener("DOMContentLoaded", function () {
         top: '45%',
         left: '60.5%',
         image: './IMG/day-1/IMG_4200.png',
+        type: 'bevinding',
+        description: '08:24 - Spaanse Wegslak'
+      },
+      {
+        top: '50%',
+        left: '62%',
+        image: './IMG/day-1/IMG_4210.png',
         type: 'bevinding',
         description: '08:24 - Spaanse Wegslak'
       },
@@ -103,7 +118,7 @@ document.addEventListener("DOMContentLoaded", function () {
         left: '60.5%',
         image: './IMG/day-1/IMG_4204.png',
         type: 'bevinding',
-        description: '08:30'
+        description: '08:30 - Spaanse wegslak' 
       },
       // {
       //   top: '42%',
@@ -126,34 +141,34 @@ document.addEventListener("DOMContentLoaded", function () {
         left: '60%',
         image: './IMG/day-2/IMG_4240.png',
         type: 'bevinding',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: "07:56 - Spaanse wegslak naast Carmen's bord."
       },
       {
         top: '50%',
         left: '61%',
         image: './IMG/day-2/IMG_4244-v1.png',
         type: 'bevinding',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '07:57 - Bruine Spaanse wegslak.'
       },
       {
         top: '46%',
         left: '60%',
         image: './IMG/day-2/IMG_4245.png',
         type: 'bevinding',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '07:58 - Spaanse Wegslak.'
       },
       {
         top: '57%',
         left: '60%',
         image: './IMG/day-2/IMG_4246.png',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '08:00 - Spaanse wegslak naast een bloem.'
       },
       {
         top: '65%',
         left: '60%',
         image: './IMG/day-2/IMG_4247.jpg',
         type: 'bevinding',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '07:59 - Spaanse Wegslak.'
       },
     ],
     3: [{
@@ -161,35 +176,34 @@ document.addEventListener("DOMContentLoaded", function () {
         left: '60%',
         image: './IMG/day-3/IMG_4281.png',
         type: 'bevinding',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '08:04 - Korte bruine Spaanse Wegslak'
       },
       {
         top: '45%',
         left: '61%',
         image: './IMG/day-3/IMG_4280.png',
         type: 'bevinding',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '08:01 - Bruine Spaanse Wegslak'
       },
       {
         top: '67%',
         left: '63%',
         image: './IMG/day-3/IMG_4292.png',
         type: 'bevinding',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '08:29 - Grote Spaanse wegslak.'
       },
       {
         top: '55%',
         left: '61.5%',
         image: './IMG/day-3/IMG_4282.png',
         type: 'bevinding',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '08:22 - Samen op de foto met Rob (Schooltuin Medewerker) en een Saanse wegslak.'
       },
       {
         top: '66%',
         left: '61.5%',
         image: './IMG/day-3/IMG_4285.png',
-        type: 'foto',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        type: 'foto'
       },
     ],
     4: [{
@@ -197,35 +211,41 @@ document.addEventListener("DOMContentLoaded", function () {
         left: '46%',
         image: './IMG/day-4/IMG_4305.png',
         type: 'foto',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: 'Vandaag is het 12°C.'
       },
       {
         top: '66%',
         left: '61.5%',
         image: './IMG/day-4/IMG_4307.png',
-        type: 'foto',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        type: 'foto'
       },
       {
         top: '58%',
         left: '60%',
         image: './IMG/day-4/IMG_4308.png',
         type: 'bevinding',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '08:00 - Spaanse wegslak'
       },
       {
         top: '50%',
         left: '60.5%',
         image: './IMG/day-4/IMG_4309.png',
         type: 'foto',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '08:02 - Bladeren van een courgette plant die een beetje gegeten zijn.'
+      },
+      {
+        top: '55%',
+        left: '60.5%',
+        image: './IMG/day-4/IMG_4310.png',
+        type: 'bevinding',
+        description: '08:03 - Een Spaanse wegslak tussen de plantjes.'
       },
       {
         top: '53%',
         left: '60%',
         image: './IMG/day-4/IMG_4312.png',
         type: 'bevinding',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '08:10 - Spaanse wegslak'
       },
     ],
     5: [{
@@ -233,25 +253,110 @@ document.addEventListener("DOMContentLoaded", function () {
         left: '46%',
         image: './IMG/day-5/IMG_4330.png',
         type: 'foto',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: 'Vandaag is het 6°C'
       },
       {
         top: '50%',
         left: '60.5%',
         image: './IMG/day-5/IMG_4336.png',
         type: 'foto',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: 'De courgette plant, weinig verandering.'
       },
       {
         top: '60%',
         left: '61.5%',
         image: './IMG/day-5/IMG_4334.png',
         type: 'bevinding',
-        description: 'Dit is een beschrijving voor Marker 3.'
+        description: '08:00 - Spaanse wegslak op een tak.'
       },
 
     ],
     6: [{
+        top: '90%',
+        left: '46%',
+        image: './IMG/day-6/IMG_4363.png',
+        type: 'foto',
+        description: 'Vandaag is het 5°C'
+      },
+      {
+        top: '47%',
+        left: '74%',
+        image: './IMG/day-6/IMG_4361.png',
+        type: 'bevinding',
+        description: '07:55 - Spaanse wegslak'
+      },
+      {
+        top: '45%',
+        left: '60%',
+        image: './IMG/day-6/IMG_4359.png',
+        type: 'bevinding',
+        description: '07:50 - Spaanse wegslak'
+      },
+      {
+        top: '45%',
+        left: '73%',
+        image: './IMG/day-6/IMG_4360.png',
+        type: 'bevinding',
+        description: '07:52 - Spaanse wegslak'
+      },
+      {
+        top: '48%',
+        left: '71%',
+        image: './IMG/day-6/IMG_4362.png',
+        type: 'foto',
+        description: 'Nieuwe locatie, getoond door een medewerker van de schooltuin.'
+      },
+    ],
+    7: [{
+        top: '90%',
+        left: '46%',
+        image: './IMG/day-7/IMG_4399.png',
+        type: 'foto',
+        description: 'Vandaag is het 14°C.'
+      },
+      {
+        top: '66%',
+        left: '61.5%',
+        image: './IMG/day-7/IMG_4407.png',
+        type: 'foto'
+      },
+      {
+        top: '64%',
+        left: '61%',
+        image: './IMG/day-7/IMG_4401.png',
+        type: 'bevinding',
+        description: '08:18 - 2 Spaanse Wegslakken'
+      },
+      {
+        top: '60%',
+        left: '61%',
+        image: './IMG/day-7/IMG_4406.png',
+        type: 'bevinding',
+        description: '08:20 - Een Spaanse wegslak'
+      },
+      {
+        top: '46%',
+        left: '74%',
+        image: './IMG/day-7/IMG_4404.png',
+        type: 'bevinding',
+        description: '08:22 - Een lange Spaanse wegslak'
+      },
+      {
+        top: '46%',
+        left: '72%',
+        image: './IMG/day-7/IMG_4405.png',
+        type: 'bevinding',
+        description: '08:24 - Spaanse Wegslak mama en baby'
+      },
+      {
+        top: '43%',
+        left: '71%',
+        image: './IMG/day-7/IMG_4403.png',
+        type: 'bevinding',
+        description: '08:20 - Bruine Spaanse Wegslak'
+      },
+    ],
+    8: [{
         top: '90%',
         left: '46%',
         image: './IMG/day-6/IMG_4363.png',
@@ -392,10 +497,21 @@ document.addEventListener("DOMContentLoaded", function () {
   buttons.forEach(button => {
     button.addEventListener('click', function () {
       const day = this.getAttribute('data-day');
+      console.log(`Dag ${day} is geklikt`);  // Log welke dag is geklikt
+      
+      buttons.forEach(btn => btn.classList.remove('activeBtn'));  // Verwijder de class van alle knoppen
       updateNavBar(day);
       addMarkers(day); // Voeg markers toe voor de geselecteerde dag
+      this.classList.add('activeBtn');  // Voeg de active class toe aan de geklikte knop
     });
   });
+
+  const showMap = document.querySelector('.about-kalf button') 
+  const kalffMap = document.querySelector('.kaart-container')
+
+showMap.addEventListener('click', ()=>{
+  kalffMap.classList.toggle('visible')
+})
 
   // Voeg markers toe voor de eerste dag
   updateNavBar(1); // Start met dag 1
